1b72b2fd1386a597d5ce7d8384f5fc193ef99788
Ashmslmhh <ashmasalimah@gmail.com>
2024-12-27 08:46:31 +0800

    Delete Modul 0 directory

--- Modul 0/SOAL-2/PRAK002-2410817220015-Ashma-Salimah.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main () {
-    printf("Selamat Pagi, Ashma Salimah\n");
-    printf("Selamat Siang, Ashma Salimah\n");
-    printf("Selamat Malam, Ashma Salimah");
-    return 0;
-}
--- Modul 0/SOAL-2/PRAK002-2410817220015-Ashma-Salimah.py
+++
@@ -1,3 +0,0 @@
-print("Selamat Pagi, Ashma Salimah")
-print("Selamat Siang, Ashma Salimah")
-print("Selamat Malam, Ashma Salimah")
