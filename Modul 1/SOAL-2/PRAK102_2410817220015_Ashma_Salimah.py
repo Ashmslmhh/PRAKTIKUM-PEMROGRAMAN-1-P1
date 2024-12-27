644a663c222c9403b3ad622d8f7d329d666bd499
Ashmslmhh <ashmasalimah@gmail.com>
2024-12-27 08:46:48 +0800

    Delete Modul 1 directory

--- Modul 1/SOAL-2/PRAK102_2410817220015_Ashma_Salimah.py
+++
@@ -1,8 +0,0 @@
-a = 4
-b = 8
-c = 3
-hasil = 4 * 8 / 3
-print(f"Variabel a bernilai {a}")
-print(f"Variabel b bernilai {b}")
-print(f"Variabel c bernilai {c}")
-print(f"Hasil dari a dikali b dibagi c adalah {hasil:.6f}")
